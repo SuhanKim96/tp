0f28fed381f52ed4c435e0558142efe28d9d059d
suhan0906 <125735654+suhan0906@users.noreply.github.com>
2026-03-12 22:58:16 +0800

    Fix style

--- src/main/java/seedu/hireshell/logic/parser/ParserUtil.java
+++ src/main/java/seedu/hireshell/logic/parser/ParserUtil.java
@@ -9,7 +9,12 @@ import java.util.Set;
 import seedu.hireshell.commons.core.index.Index;
 import seedu.hireshell.commons.util.StringUtil;
 import seedu.hireshell.logic.parser.exceptions.ParseException;
-import seedu.hireshell.model.person.*;
+import seedu.hireshell.model.person.Email;
+import seedu.hireshell.model.person.Name;
+import seedu.hireshell.model.person.Person;
+import seedu.hireshell.model.person.Phone;
+import seedu.hireshell.model.person.Rating;
+import seedu.hireshell.model.person.Status;
 import seedu.hireshell.model.tag.Tag;
 
 /**
